8db8cce76f4e8474595b76c3d2dcd94e4e30f175
skd1993 <shobhit.kr@outlook.com>
2015-09-27 15:06:54 +0530

    full recruiter list on single page

---
+++ RMFullTable.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        RMFullTable
+// @namespace   http://tnp.dtu.ac.in
+// @description script to modify link to show full list companies
+// @include     /^http:\/\/tnp\.dtu\.ac\.in\/rm3y\/utilities\/new_rec_table\.php\?id=2k12\/se\/[0-1][0-9][0-9]&l1=0&l2=[0-9]{2}$/
+// @version     1
+// @grant       none
+// ==/UserScript==
+window.location.replace('http://tnp.dtu.ac.in/rm3y/utilities/new_rec_table.php?id=2k12/se/075&l1=0&l2=1000');
+
