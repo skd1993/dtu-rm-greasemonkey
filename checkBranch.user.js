823beabaedc1e46ad8f9627e60611863f5286831
skd1993 <shobhit.kr@outlook.com>
2015-09-27 15:02:32 +0530

    updated

--- checkBranch.user.js
+++ checkBranch.user.js
@@ -7,21 +7,25 @@
 // @grant       none
 // ==/UserScript==
 var rows = document.getElementsByTagName('tr');
+//console.log(rows);
 var len = rows.length;
+//console.log(len);
 for (var i = 1; i < len; i++)
 {
   var tr = rows[i];
   var cll = tr.cells[1].innerHTML;
   var dt = tr.cells[7].innerHTML;
+  //console.log(dt);
   var bits = dt.split('/');
-
+  //console.log(bits[0]);
   var d = Number(bits[0]) + 1;
   var col_date = new Date(bits[2], bits[1] - 1, d);
+  console.log(col_date);
   var curr_date = new Date();
-
-// change the branch name to your branch
+  //console.log(curr_date);
   if (cll.indexOf('Software') != - 1 || cll.indexOf('software') != - 1)
   {
+    //tr.style.backgroundColor = '#FF0000';
     if (col_date.getTime() > curr_date.getTime())
     {
       tr.style.backgroundColor = '#66CCFF';
@@ -32,3 +36,4 @@ for (var i = 1; i < len; i++)
     }
   }
 }
+
